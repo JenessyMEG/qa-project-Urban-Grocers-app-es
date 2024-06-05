95761e14d1eed26931ea153f85167c5391facf08
JenessyMEG <jenessymestevesg@gmail.com>
2024-06-05 02:29:40 -0300

    Update configuration.py
    Modificaciones realizadas para la primera entrega

--- configuration.py
+++ configuration.py
@@ -1,3 +1,5 @@
-URL_SERVICE = # Inserta tu dirección de URL sin la barra diagonal al final
-CREATE_USER_PATH = # Almacena la ruta para crear un usuario o usuaria en esta variable
-KITS_PATH = # Almacena la ruta para crear un kit en esta variable
+URL_SERVICE = "https://cnt-c8657179-3544-40b0-a167-18f08a1e4be9.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+CREATE_USER_PATH = "/api/v1/users/"
+CREATE_KIT_PATH = "/api/v1/kits"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
