e7338df5109b945ebad3d0830207952d7ac4ab15
JenessyMEG <jenessymestevesg@gmail.com>
2024-06-05 02:29:29 -0300

    Update data.py
    Modificaciones realizadas para la primera entrega

---
+++ data.py
@@ -0,0 +1,34 @@
+headers = {
+    "Content-Type": "application/json"
+}
+
+user_body = {
+    "firstName": "Andrea",
+    "phone": "+11234567890",
+    "address": "123 Elm Street, Hilltop"
+}
+
+kit_body = {
+       "name": "Kit picninc de noche",
+       "card": {
+           "id": 1,
+           "name": "Para la situación"
+       },
+       "productsList": [],
+       "id": 7,
+       "productsCount": 0
+   }
+
+"""
+get_new_user_token()
+
+kit_body1 = { "name": "a"}
+kit_body2 = { "name":"El valor de prueba para esta comprobación será inferior a"}
+kit_body3 = { "name": "" }
+kit_body4 = { "name":"El valor de prueba para esta comprobación será inferior a” }
+kit_body5 = { "name": ""№%@"," }
+kit_body6 = { "name": " A Aaa " }
+kit_body7 = { "name": "123" }
+kit_body8 = { }
+kit_body9 = { "name": 123 }
+"""las
