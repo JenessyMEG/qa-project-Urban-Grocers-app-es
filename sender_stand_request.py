b523667ae9a0050bfac7cba7134e4394a3a87c33
JenessyMEG <jenessymestevesg@gmail.com>
2024-06-14 21:25:11 -0300

    Update sender_stand_request.py
    Modificacion del authtoken

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -9,8 +9,6 @@ def post_new_user(body):
                          headers=data.headers)
 
 response = post_new_user(data.user_body);
-print(response.status_code)
-print(response.json())
 
 def get_new_user_token():
     response = post_new_user((data.user_body))
@@ -19,10 +17,9 @@ def get_new_user_token():
 
 def post_new_client_kit(kit_body):
     auth_token = get_new_user_token()
-    authorization = {
-        "Content-Type": "application/json",
-        "Authorization": f'Bearer {auth_token}'
-}
+    authorization  = data.headers
+    authorization ["Authorization"] = f"Bearer {token}"
+    return requests.post(configuration.URL_SERVICE + configuration.KITS_PATH, json=bodykit, headers=authorization)
     return requests.post(configuration.URL_SERVICE + configuration.CREATE_KIT_PATH,
                          json=kit_body, #Cuerpo de la solicitud
                          headers= authorization) #Encabezado de la solicitud
